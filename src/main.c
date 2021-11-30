648f86a9b2c041ef16256b1c1c572a7d218620f1
MasterSev z Madarska <metrohonza5@gmail.com>
2021-11-30 13:33:01 +0100

    ze skoly

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "stm8s.h"
 #include "milis.h"
-
+#include "stm8s_gpio.h"
+#include "stm8_hd44780.h"
 /*#include "delay.h"*/
 /*#include <stdio.h>*/
 /*#include "../lib/uart.c"*/
@@ -10,19 +11,20 @@
 
 #define LED_PORT GPIOC
 #define LED_PIN  GPIO_PIN_5
-#define LED_ON   GPIO_WriteHigh(LED_PORT, LED_PIN);
-#define LED_OFF  GPIO_WriteLow(LED_PORT, LED_PIN);
-#define LED_FLIP GPIO_WriteReverse(LED_PORT, LED_PIN);
+#define LED_ON   GPIO_WriteHigh(LED_PORT, LED_PIN)
+#define LED_OFF  GPIO_WriteLow(LED_PORT, LED_PIN)
+#define LED_REVERSE GPIO_WriteReverse(LED_PORT, LED_PIN)
 
 #define BTN_PORT GPIOE
 #define BTN_PIN  GPIO_PIN_4
-#define BTN_PUSH (GPIO_ReadInputPin(BTN_PORT, BTN_PIN)==RESET) 
+#define BTN_PUSH (GPIO_ReadInputPin(BTN_PORT, BTN_PIN)==RESET) //porovnani tlacitko(zmackle /neni) vraci 1/0
 
 
 void init(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
     GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT); 
     init_milis();
 }
 
@@ -38,6 +40,7 @@ int main(void)
         if (milis() - time > 333 && BTN_PUSH) {
             GPIO_WriteReverse(LED_PORT, LED_PIN);
             time = milis();
+            LED_REVERSE;
         }
 
         /*LED_FLIP; */
